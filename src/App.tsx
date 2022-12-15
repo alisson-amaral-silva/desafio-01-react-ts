add096c88851155382dc368c282016d16686d0ea
Alisson Amaral <aalissonamaral98@gmail.com>
2022-12-15 12:30:52 -0300

    criação do componente header e sua estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css'
+import { Header } from './components/Header'
 import './global.css'
 
 function App() {
-  return <h1>Deu bom</h1>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App
