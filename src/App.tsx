60736537e17dff52f88657ff42451fe9857cb9c2
Alisson Amaral <aalissonamaral98@gmail.com>
2022-12-16 00:00:15 -0300

    criação do componente checkbox, sua estilização e criação de cada item na lista de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import styles from './App.module.css'
 import { Header } from './components/Header'
 import { AddTask } from './components/AddTask'
 import './global.css'
-import { TaskList } from './components/TaskList'
+import { TaskList, Tasks } from './components/TaskList'
 
 function App() {
+  const tasks = [
+    {
+      id: 1,
+      description: 'Estudar ReactJS',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      description: 'Estudar ReactJS',
+      isCompleted: true
+    }
+  ] as Tasks[]
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
         <AddTask />
         <main>
-          <TaskList />
+          <TaskList tasks={tasks} />
         </main>
       </div>
     </>
--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,5 +1,7 @@
 import styles from './TaskList.module.css'
 import clipboard from '../../assets/clipboard.svg'
+import { Trash } from 'phosphor-react'
+import { Checkbox } from '../Checkbox'
 
 export interface Tasks {
   id: number
@@ -24,7 +26,30 @@ export function TaskList({ tasks }: TaskListProps) {
           <span>2 de 5</span>
         </strong>
       </header>
-      {!tasks?.length && (
+      {tasks?.length ? (
+        <div className={styles.listWrapper}>
+          {tasks.map((task, index) => {
+            return (
+              <div className={styles.boxWrapper}>
+                <Checkbox id={`${index}`} />
+                <span
+                  className={
+                    task.isCompleted
+                      ? styles.completedTaskText
+                      : styles.defaultTaskText
+                  }
+                >
+                  {task.description}
+                </span>
+
+                <button>
+                  <Trash size={20} />
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      ) : (
         <div className={styles.emptyListWrapper}>
           <img src={clipboard} alt="icone de lista de itens" />
           <strong>Você ainda não tem tarefas cadastradas</strong>
