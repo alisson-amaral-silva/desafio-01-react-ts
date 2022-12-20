428faa6f801e67c4cb438cd0ad97e00c4bb4561b
Alisson Amaral <aalissonamaral98@gmail.com>
2022-12-20 11:21:10 -0300

    removendo codigo desnecessario

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,6 @@ import './global.css'
 import { Task } from './components/Task'
 
 function App() {
-  // const tasks = [
-  //   {
-  //     id: 1,
-  //     description: 'Estudar ReactJS',
-  //     isCompleted: false
-  //   },
-  //   {
-  //     id: 2,
-  //     description: 'Estudar ReactJS',
-  //     isCompleted: true
-  //   }
-  // ] as Tasks[]
-
-
 
   return (
     <>
